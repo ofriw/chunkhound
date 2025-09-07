b388b6713e48d763014ec8966e17bf6a18c68714
Ofri Wolfus <ofri.wolfus@gmail.com>
2025-09-07 13:41:12 +0300

    Export text input utilities in CLI utils module

--- chunkhound/api/cli/utils/__init__.py
+++ chunkhound/api/cli/utils/__init__.py
@@ -1,6 +1,7 @@
 """Shared utilities for ChunkHound CLI commands."""
 
 from .rich_output import RichOutputFormatter, format_health_status, format_stats
+from .text_input import TextInputState, create_text_input_display
 from .validation import validate_config_args, validate_path, validate_provider_args
 
 __all__ = [
@@ -10,4 +11,6 @@ __all__ = [
     "validate_path",
     "validate_provider_args",
     "validate_config_args",
+    "TextInputState",
+    "create_text_input_display",
 ]
